aaeb510d24351477a77d177dcd341bd508f5aba7
Mikhail Sazanof <m@sazanof.ru>
2025-08-26 00:55:05 +0300

    Add Copyright

--- services/cameras/classes/camera_stream.py
+++ services/cameras/classes/camera_stream.py
@@ -1,3 +1,18 @@
+#  Copyright (C) 2025 Mikhail Sazanov
+#  #
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  #
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#  #
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 import datetime
 import os
@@ -212,7 +227,6 @@ class CameraStream:
             self.time_part_start = 0
 
         if isinstance(self.tracker, ROITracker):
-            Logger.debug(f'[{self.camera.name}] Update areas for camera')
             self.tracker.update_all_rois(self.camera.areas)
 
         self.need_skip = False
